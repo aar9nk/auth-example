8ceba9ba77e2fc25a86de173291b4362d9ae4660
Aaron Byrom <aaronbyrom@protonmail.com>
2022-01-25 18:37:24 +1100

    add sign in / verify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+    loading: false,
+  });
+  
+  const [formStateVerify, setFormStateVerify] = useState({
+    email: "",
+    code: "",
+    loading: false,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          borderBottom: "1px solid black",
+          width: '100%',
+        }}
+      >
+        <h3>Sign up</h3>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "350px",
+          }}
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setFormState({
+              email: formState.email,
+              password: formState.password,
+              loading: true,
+            });
+
+            console.log(
+              "Form email",
+              formState.email,
+              "Form password",
+              formState.password
+            );
+
+            await Auth.signUp({
+              username: formState.email,
+              password: formState.password,
+              attributes: {
+                email: formState.email,
+              }
+            });
+
+            setFormState({
+              email: formState.email,
+              password: formState.password,
+              loading: false,
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            placeholder="email"
+            type="email"
+            onChange={(e) =>
+              setFormState({
+                email: e.target.value,
+                password: formState.password,
+                loading: formState.loading,
+              })
+            }
+          />
+          <input
+            placeholder="password"
+            type="password"
+            onChange={(e) =>
+              setFormState({
+                email: formState.email,
+                password: e.target.value,
+                loading: formState.loading,
+              })
+            }
+          />
+          <button>Sign up</button>
+        </form>
+        </div>
+        <div style={{
+            borderBottom: "1px solid black",
+            width: '100%',
+            display: "flex",
+            alignItems: 'center',
+            flexDirection: "column",
+        }}>
+        <h3>Verify</h3>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "350px",
+          }}
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setFormStateVerify({
+              email: formStateVerify.email,
+              code: formStateVerify.code,
+              loading: true,
+            });
+
+            console.log(
+              "Form email",
+              formStateVerify.email,
+              "Form code",
+              formStateVerify.code
+            );
+
+            await Auth.confirmSignUp(formStateVerify.email, formStateVerify.code);
+
+            setFormStateVerify({
+              email: formStateVerify.email,
+              code: formStateVerify.code,
+              loading: false,
+            });
+          }}
+        >
+          <input
+            placeholder="email"
+            type="email"
+            onChange={(e) =>
+              setFormStateVerify({
+                email: e.target.value,
+                code: formStateVerify.code,
+                loading: formStateVerify.loading,
+              })
+            }
+          />
+          <input
+            placeholder="code"
+            type="code"
+            onChange={(e) =>
+              setFormStateVerify({
+                email: formStateVerify.email,
+                code: e.target.value,
+                loading: formStateVerify.loading,
+              })
+            }
+          />
+          <button>Verify</button>
+        </form>
+      </div>
     </div>
   );
 }
